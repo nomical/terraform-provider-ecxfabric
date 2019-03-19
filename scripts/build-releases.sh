da01d2a498e062c028ea2dc8bdeeeae29f51c885
Jon Dunning <jon.dunning@nomical.com>
2019-03-19 15:50:32 +0000

    minor: v0.1.0 release

--- scripts/build-releases.sh
+++ scripts/build-releases.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# Exit script if you try to use an uninitialized variable.
 set -o nounset
-
-# Exit script if a statement returns a non-true return value.
 set -o errexit
-
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
 DIST_PATH=$1
