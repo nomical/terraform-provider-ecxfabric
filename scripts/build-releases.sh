6955dda236004c13a38251dbba6f15919ac22d64
Jon Dunning <jon.dunning@nomical.com>
2022-04-08 11:41:29 +0100

    Stash

--- scripts/build-releases.sh
+++ scripts/build-releases.sh
@@ -8,7 +8,7 @@ DIST_PATH=$1
 VERSION=$2
 
 # gox -os="linux darwin windows" -arch="amd64" -output="$DIST_PATH/{{.OS}}_{{.Arch}}"
-CGO_ENABLED=0 gox -os="darwin" -arch="arm64" -output="$DIST_PATH/{{.OS}}_{{.Arch}}"
+gox -os="darwin" -arch="arm64" -output="$DIST_PATH/{{.OS}}_{{.Arch}}"
 
 cd $DIST_PATH
 
